8689833be611431d535b49cf294e16ceec4679df
liquor-vague <54058974+liquor-vague@users.noreply.github.com>
2023-07-29 14:35:31 +0800

    Add files via upload

---
+++ 组合数学/公平组合游戏（博弈论）/巴什游戏.cpp
@@ -0,0 +1,21 @@
+//luogu p4018
+//∞Õ ≤”Œœ∑ 
+#include <bits/stdc++.h>
+using namespace std;
+const int N =1e5+5;
+typedef long long ll;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(false);
+	int T;
+	cin>>T;
+	while(T--) {
+		int n;
+		cin>>n;
+		if(n%6==0)cout<<"Roy wins!"<<endl;
+		else cout<<"October wins!"<<endl;
+	}
+	
+	return 0;
+}
+
